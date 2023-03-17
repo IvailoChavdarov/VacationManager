476d5d473b2535d7cb46bcee90104020567603e4
ivailochav <ivailochav@gmail.com>
2023-03-17 14:57:38 +0200

    Extended user class and added user roles

---
+++ Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace VacationManager.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [MaxLength(70)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(70)]
+        public string LastName { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using VacationManager.Data;
+using VacationManager.Models;
 
 namespace VacationManager
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public async static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +17,28 @@ namespace VacationManager
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var _db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                string[] roles = new string[] { "CEO", "Developer", "Team Lead", "Unassigned" };
+                foreach (var role in roles)
+                {
+                    bool exists = await roleManager.RoleExistsAsync(role);
+                    if (!exists)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+                await _db.SaveChangesAsync();
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
